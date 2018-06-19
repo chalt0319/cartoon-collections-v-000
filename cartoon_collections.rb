a01287e109b8532ab33c5748b74e0f85d69f9ecb
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-19 12:33:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(names)
   names.each_with_index do |dwarf, index|
     puts "#{index + 1} #{dwarf}"
+  end 
 end
 
 def summon_captain_planet# code an argument here
