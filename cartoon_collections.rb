c36a23bdb05e2efb4c6a5252820ece60722add28
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-19 13:20:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     if cheese_types.detect do |snack|
       array.include?(snack)
-        end 
       return snack 
+    end 
     end 
 end
