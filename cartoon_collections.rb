f0746313b5d788d4983110a8af60beb63490bf87
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-19 12:50:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(calls)
   end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.include? do |snack|
+    
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
